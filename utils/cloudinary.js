124407c623793ff7f23c165d79d542f89ead667d
abhijithmk <abhijithj331@gmail.com>
2023-03-23 14:07:37 +0530

    first

---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "ds4kkm7c3",
+  api_key: "314277575646188",
+  api_secret: "NDTAwXcsQbtds3mGAaAZqnFD-yU",
+});
+
+export default cloudinary;
